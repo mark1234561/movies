38c2fe5fd05daf8c1f78086767e28c7736045f4a
Mark Drabant <mark@tweex.hu>
2023-06-08 14:33:05 +0200

    EnableCaching

--- src/main/java/com/example/movies/MoviesApplication.java
+++ src/main/java/com/example/movies/MoviesApplication.java
@@ -3,12 +3,13 @@ package com.example.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages="com.example.movies.entity")
 @EnableJpaRepositories
-//@EnableCaching
+@EnableCaching
 public class MoviesApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
